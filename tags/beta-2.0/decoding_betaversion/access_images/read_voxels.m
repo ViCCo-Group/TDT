6b1e7ca0e57deadffa6d120452a533aa5ac6c84c
Kai Goergen <ckai1@users.sourceforge.net>
2012-04-12 13:44:31 +0000

    deleted old beta-2.0 tag
    git-svn-id: https://svn.code.sf.net/p/decodingtoolbox/code@11 011c98f9-dede-4aae-9279-1b967127d726

--- tags/beta-2.0/decoding_betaversion/access_images/read_voxels.m
+++
@@ -1,8 +0,0 @@
-function varargout = read_voxels(software,varargin)
-
-%   read_voxels varargin: 
-%       inputs: header, coordinates (n x 3 (XYZ))
-%       output: 1 x n vector of voxel values
-
-fname = [mfilename '_' lower(software)];
-varargout{1} = feval(fname,varargin{:});
--- tags/beta-2.0/decoding_betaversion/access_images/write_image.m
+++
@@ -1,8 +0,0 @@
-function varargout = write_image(software,varargin)
-
-%   write_image varargin:
-%       inputs: header, volume (X x Y x Z)
-%       output: written header (normally not needed)
-
-fname = [mfilename '_' lower(software)];
-varargout{1} = feval(fname,varargin{:});

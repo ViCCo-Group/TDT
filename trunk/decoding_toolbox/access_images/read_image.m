18409126a84a3a38e30863998c3b4f4137a392b6
Kai Goergen <ckai1@users.sourceforge.net>
2015-08-20 16:25:15 +0000

    Strongly improved help for standard read and write file access functions (read_header.m, read_image.m, write_image.m)
    git-svn-id: https://svn.code.sf.net/p/decodingtoolbox/code@236 011c98f9-dede-4aae-9279-1b967127d726

--- trunk/decoding_toolbox/access_images/read_image.m
+++ trunk/decoding_toolbox/access_images/read_image.m
@@ -1,8 +1,22 @@
 function varargout = read_image(software,varargin)
 
-%   read_image varargin:
-%       input: header (struct variable generated with read_header)
-%       output: image in neurological space (left = left, view from top)
+% function varargout = read_image(software,varargin)
+%   IN
+%     software: String with software to read header in cfg.software (e.g. 'SPM8')
+%     varargin: header (struct variable generated with read_header) and
+%       potentially more
+%
+%   OUT
+%       image as 3D volume image in neurological space (left = left, view from top)
+%
+% EXAMPLE 
+% to read the files 'test.img' and 'test.hdr':
+%
+%   cfg = decoding_defaults;
+%   hdr = read_header(cfg.software, 'test.img');
+%   vol = read_image(cfg.software, hdr);
+
+
 
 check_software(software);
 fname = [mfilename '_' lower(software)];

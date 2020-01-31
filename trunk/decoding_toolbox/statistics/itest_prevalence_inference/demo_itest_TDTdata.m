2084abb092de67a09595bb1cda3ebc3681e1aa0f
Kai Goergen <ckai1@users.sourceforge.net>
2020-01-31 16:19:27 +0000

    Added i-test (formerly ipipi) after receiving Satoshi Hirose ok. Also added license etc.
    git-svn-id: https://svn.code.sf.net/p/decodingtoolbox/code@347 011c98f9-dede-4aae-9279-1b967127d726

--- trunk/decoding_toolbox/statistics/itest_prevalence_inference/demo_itest_TDTdata.m
+++ trunk/decoding_toolbox/statistics/itest_prevalence_inference/demo_itest_TDTdata.m
@@ -1,36 +1,60 @@
-% This demo shows how to apply iPIPI inference as statistics. It 
-% takes SPM images (.nii or .img) or TDT .mat result as input (works for 
-% searchlight, ROI or wholebrain analysis). Further explanations about the 
-% analysis in the paper below.
+% This demo shows how to apply the i-test by Satoshi Hirose (Hirose, S, 
+% 2020), a type of prevalence inference (Allefeld, Goergen, Haynes, 2016). 
+% 
+% The i-test TDT adaptation takes SPM images (.nii or .img) or TDT .mat 
+% result as input  (works for searchlight, ROI or wholebrain analysis). 
 %
-% To use this demo, first processes the TDT demo data according to
-%   demo8_demodata_decoding_tutorial_motion_direction.m 
-% that can be found in the demo directory of the tdt toolbox.
+% i-test can in general perform prevalence inference either using the 
+% empirical permutation distribution or parametric assumptions as null 
+% hypothesis. The parametric test is not implemented in this demo yet (see
+% itest.m and the publication for that).
 %
-% Note: This is really just a demo. It is statistically invalid (because we 
-% have only two demo data sets). It demonstrates however how to use it.
+% To use this demo, either
+%   a. download the demo data from 
+%        https://sites.google.com/site/tdtdecodingtoolbox/home/download
+% or 
+%   b. create the demo data using 
+%         demo8_demodata_decoding_tutorial_motion_direction.m 
+%      from demo directory of tdt.
+% or
+%   c. figure out how to use a parametric assumption instead of the 
+%       empirical permutation distribution (in this case, please notice us)
 %
-% Please CITE iPIPI as: 
-%   Hirose, S. (2019). Valid and powerful statistical test for decoding 
-%     accuracy—Proposal of Permutation-based Information Prevalence 
-%     Inference using the i-th order statistic. BioRxiv, 578930. 
-%  https://doi.org/10.1101/578930
+% Note: This demo is really just a demo how to use the code. Using the 
+% prepared set is statistically invalid, because we have use the data for 
+% the same participant as if it were the data of different participants.
+% MAKE SURE that the data of different participants is loaded for your
+% data.
 %
-% For more explanation on iPIPI, see reference above.
 %
-% Author: Demos and adaption to TDT by Kai, original ipipi.m code by 
-%   Satoshi Hirose
+% Please CITE i-test as: 
+% Hirose, S. (2020). Valid and powerful group statistics for decoding 
+%   accuracy: Information Prevalence Inference using the i-th order 
+%   statistic (i-test). BioRxiv, 578930. https://doi.org/10.1101/578930
 %
-% The iPIPI implementation in TDT is in an experimental  state. It has not 
+% and prevalence inference analyis as:
+% Allefeld, C., Goergen, K., & Haynes, J.-D. (2016). 
+%   Valid population inference for information-based imaging: From the 
+%   second-level t-test to prevalence inference. NeuroImage. 
+%   http://doi.org/10.1016/j.neuroimage.2016.07.040
+%
+%
+% Authors: 
+%   original itest.m code by Satoshi Hirose, 
+%   Demo and adaption to TDT by Kai
+%
+% itest.m was retrieved from https://github.com/satoshi-hirose/i-test/
+% i-test has been released under GPL3 (see itest-core/LICENSE)
+%
+% REMARK:
+% The i-test implementation in TDT is in an experimental state. It has not 
 % been extensively tested by the authors of TDT. Use at own risk.
-%
-% Author of this demo: Kai
-%
-% HIST:
-%   2020/01/15: Version 1 for TDT based demo_pervalence_TDT.m
 %
 % DISCLAIMER: This function is in beta stage. It seem to work as it should,
 %   but has not been extensively tested by the public, thus use with care.
+
+% HIST:
+%   2020/01/31: itest version 0.1 for TDT based on demo_pervalence_TDT.m
 
 %% Check that SPM and TDT are available on the path
 
@@ -103,7 +127,7 @@ for sbj = 1:n_sbjs
     % get the original unpermuted result image as first image (required by the package)
     orig_image = cellstr(spm_select('FPList',orig_inputdir{sbj},orig_filemask{sbj}));
     if length(orig_image) ~= 1
-        error('There should be exactly 1 unpermuted input file for %s, but we found %i, please check', orig_image, length(orig_image))
+        error('There should be exactly 1 unpermuted input file for %s, but we found %i, please check', orig_image, length(orig_image)) %#ok<PFCEL>
     elseif isempty(orig_image{1})
         error('No file found for %s %s, please check', orig_inputdir{sbj}, orig_filemask{sbj}, length(orig_image))
     end
@@ -124,7 +148,7 @@ warning(['In this demo, we REUSE THE PERMUTATION DATA FROM 1 SUBJECT TO SIMULATE
     'This means that in the permutations for all "subjects" will be identical.' char(10) ...
     'This is done becase we dont have multiple subjects in the TDT example dataset, but want to demonstrate how multiple subjects work.' char(10) ...
     'IN A REAL ANALYSIS YOU WILL OF COURSE NEED DIFFERENT PERMUTATION DATA FOR DIFFERENT SUBJECTS!!! ' char(10) ...
-    'For information on iPIPI, see Hirose (2019) https://doi.org/10.1101/578930'])
+    'For information on itest, see Hirose (2020) https://doi.org/10.1101/578930'])
 str = input('If you have understood the above warning, type ''yes'': ','s');
 if strcmpi(str,'yes') || strcmpi(str,'y')
     % do nothing
@@ -134,9 +158,9 @@ else
 end
 
 %% Define where to save the results
-resultdir = fullfile(orig_inputdir{1}, 'iPIPI_demo');
+resultdir = fullfile(orig_inputdir{1}, 'itest_demo');
 mkdir(resultdir);
-resultfilenames = fullfile(resultdir, 'iPIPI_demo');
+resultfilenames = fullfile(resultdir, 'itest_demo');
 disp(['Writing result to ' resultfilenames '*.*']);
 
 %% Do the analysis
@@ -145,17 +169,16 @@ disp(['Writing result to ' resultfilenames '*.*']);
 % this moment in time will be saved as image and/or returned.
 
 % run prevalence analysis
-ipipiTDT(inputimages, resultfilenames);
+itestTDT(inputimages, resultfilenames);
 
-% The function returns images with the resuilts. See prevalenceCore.m for
-% information about the output files.
+% Without output, the function creates images with the results.
 %
 % If you really wish not to write the result images to disk, use
-% all_results = prevalence(inputimages, [], 'DONTWRITE');
+% all_results = itestTDT(inputimages, 'DONTWRITE');
 %
-% For all further options, see help prevalence.m
+% For all further options, see "help itestTDT" and "help itest"
 %
 % Enjoy!
 
-disp('iPIPI TDT demo analysis finished.')
+disp('itest TDT demo analysis finished.')
 disp(['Results in: ' resultfilenames])

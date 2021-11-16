9ffc3c172e4c14d013ee01e744b5e9d2bf819917
Kai Goergen <ckai1@users.sourceforge.net>
2021-11-16 16:56:24 +0000

    renamed bug-fixed transres_confusion_matrix_plus_undecided version to 3.999E2 (E1 already existed) + improved error message for libsvm without scaling. Uploading this version now.
    git-svn-id: https://svn.code.sf.net/p/decodingtoolbox/code@380 011c98f9-dede-4aae-9279-1b967127d726

--- decoding.m
+++ decoding.m
@@ -1,6 +1,6 @@
 % function [results, cfg, passed_data, misc] = decoding(cfg, passed_data, misc)
 %
-% The Decoding Toolbox, Version:3.999E1 2021/11/16, by Martin Hebart & Kai Goergen
+% The Decoding Toolbox, Version:3.999E2 2021/11/16, by Martin Hebart & Kai Goergen
 %
 % This is the main function of The Decoding Toolbox which links to all
 % subfunctions performed for brain image decoding. This toolbox is capable
@@ -249,7 +249,7 @@ verbose = cfg.verbose;
 reports = []; % init
 
 % Display version
-ver = 'The Decoding Toolbox (by Martin Hebart & Kai Goergen), 3.999E1 2021/11/16'; % also change header of this file and in LOG.txt (maybe year in LICENSE.txt)
+ver = 'The Decoding Toolbox (by Martin Hebart & Kai Goergen), 3.999E2 2021/11/16'; % also change header of this file and in LOG.txt (maybe year in LICENSE.txt)
 cfg.info.ver = ver;
 dispv(1,ver)
 dispv(1,'Preparing analysis: ''%s''',cfg.analysis)
